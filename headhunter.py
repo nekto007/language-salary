96445bcf67960bcf66f51405c3a65122d6014263
igorkorobko <nekto.korobko@gmail.com>
2023-03-03 22:25:00 +0300

    Create function fetch_vacancies_from_hh add return in function predict_rub_salary_hh

--- headhunter.py
+++ headhunter.py
@@ -1,4 +1,5 @@
 import os
+from itertools import count
 
 from dotenv import load_dotenv
 
@@ -13,4 +14,25 @@ def predict_rub_salary_hh(vacancy: dict):
     salary_from = salary.get('from')
     salary_to = salary.get('to')
     if salary.get("currency") == "RUR":
-        predict_salary(salary_from, salary_to)
+        return predict_salary(salary_from, salary_to)
+
+
+def fetch_vacancies_from_hh(language):
+    import requests
+    url = HH_API_BASE_URL
+    vacancies = []
+    for page in count(0):
+        params = {
+            "area": 1,
+            "period": 30,
+            "only_with_salary": True,
+            "text": f'программист {language}',
+            "page": page
+        }
+        response = requests.get(url, params)
+        response.raise_for_status()
+        data_vacancies = response.json()
+        vacancies.extend(data_vacancies['items'])
+        if page >= data_vacancies['pages']:
+            break
+    return vacancies
