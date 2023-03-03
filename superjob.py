a2df7db622583aef615d16f5d63aaa8e78bc9e54
igorkorobko <nekto.korobko@gmail.com>
2023-03-03 22:23:55 +0300

    Create function fetch_vacancies_from_sj

--- superjob.py
+++ superjob.py
@@ -1,5 +1,6 @@
 import os
 
+import requests
 from dotenv import load_dotenv
 
 from analytics import predict_salary
@@ -14,3 +15,26 @@ def predict_rub_salary_sj(vacancy: dict):
     salary_to = vacancy.get('payment_to')
     if vacancy.get("currency") == "rub":
         return predict_salary(salary_from, salary_to)
+
+
+def fetch_vacancies_from_sj(language):
+    headers = {'X-Api-App-Id': SUPERJOB_TOKEN}
+    url = SJ_API_BASE_URL
+    params = {
+        'catalogues': 33,
+        'no_agreement': 1,
+        'town': 4,
+        'keyword': language,
+    }
+    page = 0
+    more_pages = True
+    vacancies = []
+    while more_pages:
+        params['page'] = page
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        data_vacancies = response.json()
+        more_pages = data_vacancies['more']
+        vacancies.extend(data_vacancies['objects'])
+        page += 1
+    return vacancies
