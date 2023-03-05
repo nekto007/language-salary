c6dfd6225991d85df11a52e3efd45486b6a7c10f
igorkorobko <nekto.korobko@gmail.com>
2023-03-05 17:37:59 +0300

    Remove using is None. Fixed name usual variables

--- analytics.py
+++ analytics.py
@@ -1,19 +1,19 @@
 def predict_salary(salary_from: int, salary_to: int):
     if salary_from and salary_to:
         return int((salary_from + salary_to) / 2)
-    elif salary_to is None:
+    elif not salary_to:
         return int(salary_from * 1.2)
-    elif salary_from is None:
+    elif not salary_from:
         return int(salary_to * 0.8)
 
 
 def get_statistics(language, vacancies, predict_salaries):
-    salary_list_filtered = [predict_salary for predict_salary in predict_salaries if predict_salary]
+    salary_filtered = [salary for salary in predict_salaries if salary]
     vacancies_found = len(vacancies)
-    vacancies_processed = len(salary_list_filtered)
+    vacancies_processed = len(salary_filtered)
 
-    if salary_list_filtered:
-        average_salary = str(sum(salary_list_filtered) // len(salary_list_filtered))
+    if salary_filtered:
+        average_salary = str(sum(salary_filtered) // len(salary_filtered))
     else:
         average_salary = '-'
 
